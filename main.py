625a854885e20f9747e5a0acfc6dbe7d3ece72a5
vpuma21 <vpuma@northpark.edu>
2026-02-18 10:23:03 -0600

    updating and worked on step 3

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ params = { # These are parameters for the request for the GEOCODE site
     "count": 1
 } 
 
-response = requests.get(GEOCODE_URL, params=params) # this will send the request
+response = requests.get(GEOCODE_URL, params=params) # this will send the request from geocode
 
 data = response.json() # this converts it to JSON
 
@@ -20,20 +20,20 @@ country = data ["results"][0]["country"]
 latitude = data["results"][0]["latitude"] 
 longitude = data["results"][0]["longitude"]
 
-print("city", name)
-print("country", country)
-print("latitide", latitude) # we need to extract this
-print("longitude", longitude)# and extract this
+# print("city", name)
+# print("country", country)
+# print("latitide", latitude) # we need to extract this
+# print("longitude", longitude)# and extract this
 
 # STEP 2: FORECAST SECTION 
 
-forecast_param = { #retriving the latitude and longitude from previous step
+forecast_param = { # retriving the latitude and longitude from previous step
     "latitude": latitude,
     "longitude": longitude,
     "current_weather": True
 }
 
-response2 = requests.get(FORECAST_URL, params=forecast_param) # this will send second request
+response2 = requests.get(FORECAST_URL, params=forecast_param) # this will send second request from forecast
 
 weather_data = response2.json() # convert to JSON again for forecast
 
@@ -41,20 +41,20 @@ temperature = weather_data["current_weather"]["temperature"] # these are suppose
 windspeed = weather_data["current_weather"]["windspeed"]
 time = weather_data["current_weather"]["time"]
 
-print("Temperature:", temperature)
-print("Windspeed:", windspeed)
-print("Observation time:", time)
+# print("Temperature:", temperature)
+# print("Windspeed:", windspeed)
+# print("Observation time:", time)
 
 # STEP 3: JSON -> Python
- 
 
+result = { #the formatting on how it should be + python object
+    "city": data["results"][0]["name"],
+    "country": data["results"][0]["country"],
+    "latitude": latitude,
+    "longitude": longitude,
+    "temperature_c": temperature,
+    "windspeed_kmh": windspeed,
+    "observation_time": time
+}
 
-
-
-
-
-
-
-
-
-
+print(result) # the other print out were commented out bc those were just tests to see if it works.
