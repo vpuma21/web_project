835d6bfac118fe27a32170e03e944cfe32f98b22
vpuma21 <vpuma@northpark.edu>
2026-02-20 11:11:00 -0600

    late changes but the explanation and code for the missing points

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ import requests
 GEOCODE_URL = "https://geocoding-api.open-meteo.com/v1/search" # this will be step 1
 FORECAST_URL = "https://api.open-meteo.com/v1/forecast" # this is for step 2
 
+# May be late but working on creating a class and instantiate it with the variables obtained from the api call.
+
+class WeatherObservation:
+    def __init__(self, city, country, latitude, longitude, temperature_c, windspeed_kmh, observation_time):
+         self.city = city
+         self.country = country
+         self.latitude = latitude
+         self.longitude = longitude
+         self.temperature_c = temperature_c
+         self.windspeed_kmh = windspeed_kmh
+         self.observation_time = observation_time
+
+    def __repr__(self):
+         return(
+            "WeatherObservation("
+            f"city={self.city!r}, country={self.country!r}, "
+            f"latitude={self.latitude}, longitude={self.longitude}, "
+            f"temperature_c={self.temperature_c}, windspeed_kmh={self.windspeed_kmh}, "
+            f"observation_time={self.observation_time!r})"
+        )
+
 # STEP 1: GEOCODE SECTION
 
 params = { # These are parameters for the request for the GEOCODE site
@@ -57,6 +78,8 @@ result = { # the formatting on how it should be + python object
     "windspeed_kmh": windspeed,
     "observation_time": time
 }
+
+# supposed to create class
 
 print("Status", response.status_code) # this is the status code (an extra filler I wanted to add c:) should print that status is 200
 print(result) # the other print out were commented out bc those were just tests to see if it works.
