f134d3b24c452e36e8d63276fedcedf07c4f4f04
vpuma21 <vpuma@northpark.edu>
2026-03-05 14:15:35 -0600

    this is the app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask
-from routes import routes
+import routes
 
 app = Flask(__name__)
 
-# register all CRUD routes
-app.register_blueprint(routes)
-
-@app.route("/")
-def home():
-    return "<h1>Weather API server running</h1>"
+# register routes
+routes.register_routes(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
